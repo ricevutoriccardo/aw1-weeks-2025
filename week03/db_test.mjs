5da1b2ae5a0cac13d1b189353ed9a3adf2570569
Riccardo <riccardo.ricevuto@costa.it>
2025-08-06 00:37:16 +0200

    lessons until week 4

--- week03/db_test.mjs
+++ week03/db_test.mjs
@@ -10,10 +10,11 @@ let results = [];
 db.all(sql, (err, rows) => {
   if (err) throw err;
   for (let row of rows)
-    //console.log(row);
+    // console.log(row);
     results.push(row);
 });
 
+// essendo asincrono, non posso usare console.log(results) qui, devo utillizare quello di sopra
 console.log("******");
 for (let r of results) {
   console.log(r);
